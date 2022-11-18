31d30e7866212b2273c66277cb338639b032612b
octopus7 <octopus7net@gmail.com>
2022-11-19 06:13:46 +0900

    blank project

---
+++ blankcpp5/Source/blankcpp5/blankcpp5.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "blankcpp5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, blankcpp5, "blankcpp5" );
---
+++ blankcpp5/Source/blankcpp5/blankcpp5GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "blankcpp5GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLANKCPP5_API Ablankcpp5GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
